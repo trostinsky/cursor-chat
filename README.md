15424500b7db4fe9e1eff369c40e3a6d4c612e33
Vlad <gribennikov.vlad@gmail.com>
2019-01-17 18:49:52 +0200

    Initial for Chat

---
+++ README.md
@@ -0,0 +1,4 @@
+#Simple example of chat
+## For Start use this commands:
+– npm run server
+– npm start
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require("socket.io")(http, {
+    path: '/chat/',
+});
+
+const PORT = 9999;
+
+io.on('connection', (socket) => {
+    console.log("User connected");
+socket.on("disconnect", () => {
+    console.log("disconnected");
+});
+socket.on("message", (message) => {
+    console.log(`message: ${message}`);
+socket.broadcast.emit("new-message", message);
+})
+})
+
+app.use(express.static('../build'));
+http.listen(PORT, () => {
+    console.log(`Server is started on port №${PORT}`);
+});
